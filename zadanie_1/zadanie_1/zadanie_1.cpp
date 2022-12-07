f34fc1859ef1145a93dc7cc8dad9de4d2825bcb8
Enea <jorgji.enea@gmail.com>
2022-12-07 21:30:15 +0100

    completed condition of checking 2 greater numbers

--- zadanie_1/zadanie_1/zadanie_1.cpp
+++ zadanie_1/zadanie_1/zadanie_1.cpp
@@ -2,48 +2,37 @@
 using namespace std;
 int main()
 {
-    int a, b, c, d;
-    int max_1 = a, max_2 = b;
+    int a, b, c, d, max_1, max_2;
 
     cout << "Podaj cztery liczby calkowite: \n";
     cin >> a >> b >> c >> d;
 
+    max_1 = a;
+    max_2 = b;
+
     if (b > a)
     {
-
-    }
-/*
-    if (a > b)
-    {
-        max_1 = a;
-        max_2 = b;
-        cout << "Pierwsza wieksza liczba to " << max_1 << endl;
-        cout << "Druga wieksza liczba to " << max_2 << endl;
-    }
-    else
-    {
+        max_2 = max_1;
         max_1 = b;
-        max_2 = a;
-        cout << "Pierwsza wieksza liczba to " << max_1 << endl;
-        cout << "Druga wieksza liczba to " << max_2 << endl;
     }
-*/
 
     if (c > max_1)
     {
         max_2 = max_1;
         max_1 = c;
-        cout << "Pierwsza wieksza liczba to " << max_1 << endl;
-        cout << "Druga wieksza liczba to " << max_2 << endl;
     }
-    else if (d > max_1)
+    if (d > max_1)
     {
         max_2 = max_1;
         max_1 = d;
-        cout << "Pierwsza wieksza liczba to " << max_1 << endl;
-        cout << "Druga wieksza liczba to " << max_2 << endl;
+    }
+    else if (d > max_2)
+    {
+        max_2 = d;
     }
 
-
+    cout << "Pierwsza wieksza liczba to " << max_1 << endl;
+    cout << "Druga wieksza liczba to " << max_2 << endl;
+    
     return 0;
 }
