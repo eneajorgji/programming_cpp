0293692d711eff6155c0232d9cb120950f16b1bc
Enea <jorgji.enea@gmail.com>
2022-12-13 23:06:26 +0100

    edir while loop

--- zadanie_2/zadanie_2/zadanie_2.cpp
+++ zadanie_2/zadanie_2/zadanie_2.cpp
@@ -5,8 +5,13 @@ using namespace std;
 
 int main() {
     const int D = 5;
+    int liczba_calk;
+
+    do {
+        cout << "Wprowadz liczbe: ";
+        cin >> liczba_calk;
+
+    } while (liczba_calk % D != 0)
     
-
-
     return 0;
 }
