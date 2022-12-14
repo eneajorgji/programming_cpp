17490abf82a4ece2919c397ca0d71b61177bd403
Enea <jorgji.enea@gmail.com>
2022-12-14 23:35:04 +0100

    changed algorithm for the do-while

--- zadanie_2/zadanie_2/zadanie_2.cpp
+++ zadanie_2/zadanie_2/zadanie_2.cpp
@@ -5,40 +5,48 @@ using namespace std;
 
 int main() {
     const int D = 5;
-    int liczba_calk, liczba_max, pierwsza_wprowadzona_liczba, suma_jednocyfr = 0, ilosc_jednocyfr = 0, wieksze_od_poprzednika = 0;
-    double srednia;
-    bool pierwsza_wprowadzona = true;
-
-    cout << "Wprowadz liczbe: ";
-    cin >> pierwsza_wprowadzona_liczba;
-
-    liczba_max = pierwsza_wprowadzona_liczba;
+    int tmp1 = 0, tmp2 = 0, najwieszka_liczba = 0, ilosc_jednocyfrowych = 0, suma_jednocyfrowych = 0, wieksze_liczby = 0;
+    double avg = 0;
+    bool pierwsza = true;
 
     do {
-        cout << "Wprowadz liczbe: ";
-        cin >> liczba_calk;
+        cout << "Wprowadz liczbe:";
+        cin >> tmp2;
 
-        if (liczba_calk > liczba_max) {
-            liczba_max = liczba_calk;
-            wieksze_od_poprzednika++;
+        // wyszukanie maxymalnej wprowadzonej liczby
+        if (tmp2 > najwieszka_liczba || pierwsza == true) {
+            najwieszka_liczba = tmp2;
         }
 
-        if ((pierwsza_wprowadzona_liczba >= -9 && pierwsza_wprowadzona_liczba <= 9) && (pierwsza_wprowadzona == true)) {
-            suma_jednocyfr += pierwsza_wprowadzona_liczba;
-            ilosc_jednocyfr++;
-            pierwsza_wprowadzona = false;
+        // wyszukanie sk³adowych do œredniej liczb jednocyfrowych 
+        if (tmp2 >= -9 && tmp2 <= 9) {
+            suma_jednocyfrowych += tmp2;
+            ilosc_jednocyfrowych++;
         }
 
-        if (liczba_calk >= -9 && liczba_calk <= 9) {
-            suma_jednocyfr += liczba_calk;
-            ilosc_jednocyfr++;
+        // sprawdzenie czy ju¿ by³a wprowadzona jaka liczba
+        if (pierwsza == true) {
+            tmp1 = tmp2;
+            pierwsza = false;
         }
 
-    } while (liczba_calk % D != 0);
+        // zliczanie liczb wiêkszych od poprzednika 
+        if (tmp2 > tmp1) {
+            wieksze_liczby++;
+        }
 
-    cout << "Najwieksza liczba: " << liczba_max << endl;
-    srednia = suma_jednocyfr / (1.0 * ilosc_jednocyfr);
-    cout << "Srednia liczb jednocyfrowych: " << srednia << endl;
-    cout << "Ilosc wiekszych liczb od poprzednika: " << wieksze_od_poprzednika << endl;
+        //zachowanie ostatnio wprowadzonej liczby przed nastêpnym wprowadzeniem
+        tmp1 = tmp2;
+
+    } while (tmp2 % D != 0);
+    cout << "Najwieksza liczba: " << najwieszka_liczba << endl;
+    avg = double(suma_jednocyfrowych) / ilosc_jednocyfrowych;
+    if (ilosc_jednocyfrowych == 0) {
+        cout << "Nie ma zadnej liczby jednocyfrowej." << end;
+    }
+    else {
+        cout << "Srednia liczb jednocyfrowych: " << avg << endl;
+    }
+    cout << "Ilosc wiekszych liczb od poprzednika: " << wieksze_liczby << endl;
     return 0;
 }
