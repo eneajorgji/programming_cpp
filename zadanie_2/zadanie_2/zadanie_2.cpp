a312f19e704b2a0f9076d94fe4768f1e4adab2c1
Enea <jorgji.enea@gmail.com>
2022-12-14 15:01:44 +0100

    format

--- zadanie_2/zadanie_2/zadanie_2.cpp
+++ zadanie_2/zadanie_2/zadanie_2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     const int D = 5;
-    int liczba_calk, liczba_max, pierwsza_wprowadzona_liczba, suma_jednocyfr = 0, ilosc_jednocyfr = 0, wieksze_od_poprzed = 0;
+    int liczba_calk, liczba_max, pierwsza_wprowadzona_liczba, suma_jednocyfr = 0, ilosc_jednocyfr = 0, wieksze_od_poprzednika = 0;
     double srednia;
     bool pierwsza_wprowadzona = true;
 
@@ -20,18 +20,18 @@ int main() {
 
         if (liczba_calk > liczba_max) {
             liczba_max = liczba_calk;
-            wieksze_od_poprzed = wieksze_od_poprzed + 1;
+            wieksze_od_poprzednika++;
         }
 
         if ((pierwsza_wprowadzona_liczba >= -9 && pierwsza_wprowadzona_liczba <= 9) && (pierwsza_wprowadzona == true)) {
-            suma_jednocyfr = suma_jednocyfr + pierwsza_wprowadzona_liczba;
-            ilosc_jednocyfr = ilosc_jednocyfr + 1;
+            suma_jednocyfr += pierwsza_wprowadzona_liczba;
+            ilosc_jednocyfr++;
             pierwsza_wprowadzona = false;
         }
 
         if (liczba_calk >= -9 && liczba_calk <= 9) {
-            suma_jednocyfr = suma_jednocyfr + liczba_calk;
-            ilosc_jednocyfr = ilosc_jednocyfr + 1;
+            suma_jednocyfr += liczba_calk;
+            ilosc_jednocyfr++;
         }
 
     } while (liczba_calk % D != 0);
@@ -39,8 +39,6 @@ int main() {
     cout << "Najwieksza liczba: " << liczba_max << endl;
     srednia = suma_jednocyfr / (1.0 * ilosc_jednocyfr);
     cout << "Srednia liczb jednocyfrowych: " << srednia << endl;
-    cout << "Ilosc wiekszych liczb od poprzednika: " << wieksze_od_poprzed << endl;
+    cout << "Ilosc wiekszych liczb od poprzednika: " << wieksze_od_poprzednika << endl;
     return 0;
 }
-
-// pierwsza_wprowadzona
