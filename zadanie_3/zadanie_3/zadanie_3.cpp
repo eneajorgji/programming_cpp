54d4981753eb0988bf9a3f306d3c65f95b398515
Enea <jorgji.enea@gmail.com>
2022-12-28 21:43:08 +0100

    solved

--- zadanie_3/zadanie_3/zadanie_3.cpp
+++ zadanie_3/zadanie_3/zadanie_3.cpp
@@ -1,20 +1,67 @@
-// zadanie_3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstdlib>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+const int MAX = 100;
+const int N = 4;
+
+int main() {
+    int k, m = 0, suma = 0;
+    string tmp_napis;
+    string tnapis[MAX];
+
+    char tablica_c[N];
+
+    // podpunkt A
+    do {
+        cout << "Wprowadz liczbe calkowita k: " << endl;
+        cin >> k;
+    } while (k < 0 && k >= 9);
+
+    // podpunkt B
+    for (int i = 0; i < k; i++) {
+        cout << "Wprowadz napis nr " << i + 1 << " z " << k << endl;
+        cin >> tnapis[i];
+        tmp_napis = tnapis[i];
+        if (tmp_napis[tmp_napis.length()] = tmp_napis[0]) {
+            suma = suma + tmp_napis.length();
+            m++;
+        }
+    }
+    cout << "Srednia: " << 1.0 * suma / m << endl;
+
+    // podpunkt C
+/*
+    for (int i = 0; i < N; i++){
+        cout << "Wprowadz "<< i + 1 << " element tablicy" << endl;
+        cin>>tablica_c[i];
+    }
+
+    for(int i = 0; i < N; i++){
+        cout << tablica_c[i]<<" ";
+    }
+*/
+    for (int i = 0; i < N; i++) {
+        cout << "Wprowadz " << i + 1 << " element tablicy" << endl;
+        cin >> tablica_c[i];
+    }
+
+    cout << "Drukuj tablica_c: " << tablica_c;
+
+    for (int cykl = 0; cykl < N; cykl++) {
+        char temp = tablica_c[N - 1]; //zapisuje ostatnie element
+
+        for (int i = (N - 1); i >= 0; i--) {
+            tablica_c[i] = tablica_c[i - 1];
+        }
+        tablica_c[0] = temp;
+
+        for (int i = 0; i < N; i++) {
+            cout << tablica_c[i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
