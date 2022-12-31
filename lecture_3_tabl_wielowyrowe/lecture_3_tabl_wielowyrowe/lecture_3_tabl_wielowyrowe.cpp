9cdb0f121c8ba5969e2ade150cb9d1e13af4dc86
Enea <jorgji.enea@gmail.com>
2022-12-31 23:13:58 +0100

    added another ex

---
+++ lecture_3_tabl_wielowyrowe/lecture_3_tabl_wielowyrowe/lecture_3_tabl_wielowyrowe.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    const int w = 3, k = 4;
+    cout << "Hello World!\n";
+    int A[w][k];
+
+    cout << "podaj A: ";
+    cin >> A
+
+    cout << A << endl;
+    return 0;
+}
