ec7d9e3781e1b7e66e9e6169fe34d61fc47ff976
Enea <jorgji.enea@gmail.com>
2023-01-08 16:39:46 +0100

    editing

--- zadanie_4/zadanie_4/zadanie_4.cpp
+++ zadanie_4/zadanie_4/zadanie_4.cpp
@@ -5,13 +5,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Program for_3, drukujacy znaki pomiedzy 'A' i 'z'\n\n";
-    // Pêtla drukuj¹ca - tym razem jako zmienn¹ steruj¹c¹ zastosujemy
-    // zmienn¹ typu char
-    for (char zn = 'A'; zn <= 'z'; zn++)
-        cout << zn;
-
-    cout << endl;
-
+    
+    
     return 0;
 }
