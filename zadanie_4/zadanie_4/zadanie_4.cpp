b9666a31a28fb082ea744e48a33ca94d1e6a661e
Enea <jorgji.enea@gmail.com>
2023-01-08 16:38:50 +0100

    started project 4

--- zadanie_4/zadanie_4/zadanie_4.cpp
+++ zadanie_4/zadanie_4/zadanie_4.cpp
@@ -1,20 +1,17 @@
-// zadanie_4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstdlib>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Program for_3, drukujacy znaki pomiedzy 'A' i 'z'\n\n";
+    // Pêtla drukuj¹ca - tym razem jako zmienn¹ steruj¹c¹ zastosujemy
+    // zmienn¹ typu char
+    for (char zn = 'A'; zn <= 'z'; zn++)
+        cout << zn;
+
+    cout << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
