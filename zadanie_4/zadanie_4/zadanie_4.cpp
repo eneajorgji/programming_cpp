dc819bf5092c4d7117970a7a12512c8f499b0b7b
Enea <jorgji.enea@gmail.com>
2023-01-08 20:09:27 +0100

    completed point d and e

--- zadanie_4/zadanie_4/zadanie_4.cpp
+++ zadanie_4/zadanie_4/zadanie_4.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    const int W = 5, K = 5, wydruk_wiersz = W;
+    const int W = 5, K = 5, WYDRUK_WIERSZA = W;
     double A[W][K];
 
     srand(time(0));
@@ -25,11 +25,11 @@ int main()
     for (int i = 0; i < W; i++) {
         double suma_wiersza = 0.0;
         for (int j = 0; j < K; j++) {
-            cout << setw(wydruk_wiersz) << setprecision(2) << fixed << A[i][j];
+            cout << setw(WYDRUK_WIERSZA) << setprecision(2) << fixed << A[i][j];
             suma_wiersza += A[i][j];
         }
         double srednia_wiersza = suma_wiersza / (K * 1.0);
-        cout << " => Scrednia: " << setw(wydruk_wiersz) << setprecision(2) << fixed << srednia_wiersza << endl;
+        cout << " => Srednia: " << setw(WYDRUK_WIERSZA) << setprecision(2) << fixed << srednia_wiersza << endl;
     }
 
     // Punkt C
@@ -41,18 +41,33 @@ int main()
             suma_wiersza += A[i][j];
         }
         double srednia_wiersza = suma_wiersza / K;
-        for (int j = 0; j < K; l j++) {
+        for (int j = 0; j < K; j++) {
             if (A[i][j] < srednia_wiersza) {
                 A[i][j] = srednia_wiersza;
             }
         }
     }
 
+    // Punkt D
     // wydrukuje modifikowano tablice
     for (int i = 0; i < W; i++) {
         for (int j = 0; j < K; j++) {
-            cout << setw(wydruk_wiersz) << setprecision(2) << fixed << A[i][j];
+            cout << setw(WYDRUK_WIERSZA) << setprecision(2) << fixed << A[i][j];
         }
+        cout << setw(WYDRUK_WIERSZA) << endl;
+    }
+
+    //Punkt E
+    cout << "maksymalna wartosc dla kazdej kolumny" << endl;
+    for (int j = 0; j < K; j++) {
+        double wartosc_max = A[0][j];
+        for (int i = 0; i < W; i++) {
+            if (A[i][j] > wartosc_max) {
+                wartosc_max = A[i][j];
+            }
+        }
+        cout << setw(WYDRUK_WIERSZA) << setprecision(2) << fixed << wartosc_max;
+
     }
     return 0;
 }
