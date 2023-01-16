2b7d1ad3ff30a07b110a482673cf764277a90676
Enea <jorgji.enea@gmail.com>
2023-01-16 14:08:05 +0100

    info about the direction

--- zadanie_5/zadanie_5/zadanie_5.cpp
+++ zadanie_5/zadanie_5/zadanie_5.cpp
@@ -1,20 +1,24 @@
-// zadanie_5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ifstream plk_we;
+
+    //plk_we.open("daneR1txt");
+
+    plk_we.open("C:\\Users\\jorgj\\OneDrive\\Documents\\B.Eng.SC\\5. Semestr - PS 2\\1. Programowanie\\zadania_z_wykladow\\zadanie_5s\\zadanie_5\\daneEJ.txt");
+    if (!plk_we.is_open()) {
+        cout << "Blad otwarcia pliku" << endl;
+        return 1;
+    }
+    else {
+        cout << "Plik zostal wczytany" << endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
