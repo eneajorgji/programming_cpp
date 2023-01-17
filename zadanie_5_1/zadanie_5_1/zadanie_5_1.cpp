3d54ad693f7302687eac861cbf97a1717e280283
Enea <jorgji.enea@gmail.com>
2023-01-17 22:13:01 +0100

    optimesed code

--- zadanie_5_1/zadanie_5_1/zadanie_5_1.cpp
+++ zadanie_5_1/zadanie_5_1/zadanie_5_1.cpp
@@ -13,80 +13,90 @@ int main()
     double C = 11.5;
     double A[n][n];
 
-    ifstream plk_we("daneR1.txt");
+    ifstream plk_we("daneR.txt");
 
-    //tablica z elementami C
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            A[i][j] = C;
-        }
-    }
-
-    // load date from file
-    if (plk_we.is_open()) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!plk_we.eof()) {
-                    plk_we >> A[i][j];
-                }
-            }
-        }
+    if (!plk_we.is_open()) {
+        cout << "Plik \"daneR.txt\" nie istnieje." << endl;
+        return 1;
     }
     else {
+        //tablica z elementami C
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                A[i][j] = C;
+            }
+        }
+
+        // load date from file
+        //for (int i = 0; i < n; i++) {
+        //    for (int j = 0; j < n; j++) {
+        //        if (!plk_we.eof()) {
+        //            plk_we >> A[i][j];
+        //        }
+        //    }
+        //}
+
+        int i = 0, j = 0;
+        while (plk_we >> A[i][j] && i < n) {
+            j++;
+            if (j == n) {
+                j = 0;
+                i++;
+            }
+        }
+
+        //wydruk elementow tablicy
+        cout << "Tablica podstawowa: " << endl;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << setw(D) << fixed << setprecision(P) << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        //3. najmniejsza liczba na glownej przekatnej, dopisac ja do calego wiersza
+        double mniejsza_przekatna = A[0][0];
+        int najmniejsza_przekatna_wiersz = 0;
+        for (int i = 0; i < n; i++) {
+            if (A[i][i] < mniejsza_przekatna) {
+                mniejsza_przekatna = A[i][i];
+                najmniejsza_przekatna_wiersz = i;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            A[najmniejsza_przekatna_wiersz][i] = mniejsza_przekatna;
+        }
+
+        //4. zamienic miejscami skrajne elementy na drugiej przekatnej
+        double tmp = A[0][n - 1];
+        A[0][n - 1] = A[n - 1][0];
+        A[n - 1][0] = tmp;
+
+        //5. wykrud modyfiwkowanej tablicy
+        cout << "Tablica modyfikowana" << endl;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << setw(D) << fixed << setprecision(P) << A[i][j] << " ";
+
+            }
+            cout << endl;
+        }
+
+        string nazwa_plk_wy;
+
+        cout << "Podaj nazwe pliku: ";
+        cin >> nazwa_plk_wy;
+
+        ofstream plk_wy(nazwa_plk_wy);
+        for (int i = 0; i < n; i += 2) {
+            for (int j = 0; j < n; j++) {
+                plk_wy << setw(D) << fixed << setprecision(P) << A[i][i] << " ";
+            }
+            plk_wy << endl;
+        }
+        plk_wy.close();
+
+        return 0;
         cout << "Plik \"daneR.txt\" nie istnieje." << endl;
     }
-    
-
-    //wydruk elementow tablicy
-    cout << "Tablica podstawowa: " << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << setw(D) << fixed << setprecision(P) << A[i][j] << " ";
-        }
-        cout << endl;
-    }
-    
-    //3. najmniejsza liczba na glownej przekatnej, dopisac ja do calego wiersza
-    double mniejsza_przekatna = A[0][0];
-    int najmniejsza_przekatna_wiersz = 0;
-    for (int i = 0; i < n; i++) {
-        if (A[i][i] < mniejsza_przekatna) {
-            mniejsza_przekatna = A[i][i];
-            najmniejsza_przekatna_wiersz = i;
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        A[najmniejsza_przekatna_wiersz][i] = mniejsza_przekatna;
-    }
-    // ---------------------------------------------------------------- //
-    //4. zamienic miejscami skrajne elementy na drugiej przekatnej
-    double tmp = A[0][n - 1];
-    A[0][n - 1] = A[n - 1][0];
-    A[n - 1][0] = tmp;
-
-    //5. wykrud modyfiwkowanej tablicy
-    cout << "Modyfikowana tablica" << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << setw(D) << fixed << setprecision(P) << A[i][j] << " ";
-
-        }
-        cout << endl;
-    }
-
-    string nazwa_plk_wy;
-
-    cout << "Podaj nazwe pliku: ";
-    cin >> nazwa_plk_wy;
-
-    ofstream plk_wy(nazwa_plk_wy);
-    for (int i = 0; i < n; i += 2) {
-        for (int j = 0; j < n; j++) {
-            plk_wy << setw(D) << fixed << setprecision(P) << A[i][i] << " ";
-        }
-        plk_wy << endl;
-    }
-    plk_wy.close();
-
-    return 0;
 }
