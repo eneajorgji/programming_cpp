27687d973a6f9bfe28421c27f39651a5539b841e
Enea <jorgji.enea@gmail.com>
2023-01-17 23:49:37 +0100

    edited point 7

--- zadanie_5_1/zadanie_5_1/zadanie_5_1.cpp
+++ zadanie_5_1/zadanie_5_1/zadanie_5_1.cpp
@@ -9,94 +9,94 @@ using namespace std;
 
 int main()
 {
-    const int n = 10, P = 2, D = 6;
-    double C = 11.5;
-    double A[n][n];
+	const int n = 10, P = 2, D = 6;
+	double C = 11.5;
+	double A[n][n];
 
-    ifstream plk_we("daneR.txt");
+	ifstream plk_we("daneR.txt");
 
-    if (!plk_we.is_open()) {
-        cout << "Plik \"daneR.txt\" nie istnieje." << endl;
-        return 1;
-    }
-    else {
-        //tablica z elementami C
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                A[i][j] = C;
-            }
-        }
+	if (!plk_we.is_open()) {
+		cout << "Plik \"daneR.txt\" nie istnieje." << endl;
+		return 1;
+	}
+	else {
+		//tablica z elementami C
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				A[i][j] = C;
+			}
+		}
 
-        // load date from file
-        //for (int i = 0; i < n; i++) {
-        //    for (int j = 0; j < n; j++) {
-        //        if (!plk_we.eof()) {
-        //            plk_we >> A[i][j];
-        //        }
-        //    }
-        //}
+		// load date from file
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (!plk_we.eof()) {
+					plk_we >> A[i][j];
+				}
+			}
+		}
 
-        int i = 0, j = 0;
-        while (plk_we >> A[i][j] && i < n) {
-            j++;
-            if (j == n) {
-                j = 0;
-                i++;
-            }
-        }
+		/*int i = 0, j = 0;
+		while (plk_we >> A[i][j] && i < n) {
+			j++;
+			if (j == n) {
+				j = 0;
+				i++;
+			}
+		}*/
 
-        //wydruk elementow tablicy
-        cout << "Tablica podstawowa: " << endl;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cout << setw(D) << fixed << setprecision(P) << A[i][j] << " ";
-            }
-            cout << endl;
-        }
+		//wydruk elementow tablicy
+		cout << "Tablica podstawowa: " << endl;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cout << setw(D) << fixed << setprecision(P) << A[i][j] << " ";
+			}
+			cout << endl;
+		}
 
-        //3. najmniejsza liczba na glownej przekatnej, dopisac ja do calego wiersza
-        double mniejsza_przekatna = A[0][0];
-        int najmniejsza_przekatna_wiersz = 0;
-        for (int i = 0; i < n; i++) {
-            if (A[i][i] < mniejsza_przekatna) {
-                mniejsza_przekatna = A[i][i];
-                najmniejsza_przekatna_wiersz = i;
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            A[najmniejsza_przekatna_wiersz][i] = mniejsza_przekatna;
-        }
+		//3. najmniejsza liczba na glownej przekatnej, dopisac ja do calego wiersza
+		double mniejsza_przekatna = A[0][0];
+		int najmniejsza_przekatna_wiersz = 0;
+		for (int i = 0; i < n; i++) {
+			if (A[i][i] < mniejsza_przekatna) {
+				mniejsza_przekatna = A[i][i];
+				najmniejsza_przekatna_wiersz = i;
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			A[najmniejsza_przekatna_wiersz][i] = mniejsza_przekatna;
+		}
 
-        //4. zamienic miejscami skrajne elementy na drugiej przekatnej
-        double tmp = A[0][n - 1];
-        A[0][n - 1] = A[n - 1][0];
-        A[n - 1][0] = tmp;
+		//4. zamienic miejscami skrajne elementy na drugiej przekatnej
+		double tmp = A[0][n - 1];
+		A[0][n - 1] = A[n - 1][0];
+		A[n - 1][0] = tmp;
 
-        //5. wykrud modyfiwkowanej tablicy
-        cout << "Tablica modyfikowana" << endl;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cout << setw(D) << fixed << setprecision(P) << A[i][j] << " ";
+		//5. wykruduj modyfiwkowana tablica
+		cout << "Tablica modyfikowana" << endl;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cout << setw(D) << fixed << setprecision(P) << A[i][j] << " ";
 
-            }
-            cout << endl;
-        }
+			}
+			cout << endl;
+		}
 
-        string nazwa_plk_wy;
+		string nazwa_plk_wy;
 
-        cout << "Podaj nazwe pliku: ";
-        cin >> nazwa_plk_wy;
+		cout << "Podaj nazwe pliku: ";
+		cin >> nazwa_plk_wy;
 
-        ofstream plk_wy(nazwa_plk_wy);
-        for (int i = 0; i < n; i += 2) {
-            for (int j = 0; j < n; j++) {
-                plk_wy << setw(D) << fixed << setprecision(P) << A[i][i] << " ";
-            }
-            plk_wy << endl;
-        }
-        plk_wy.close();
+		ofstream plk_wy(nazwa_plk_wy);
+		for (int i = 0; i < n; i += 2) {
+			for (int j = 0; j < n; j++) {
+				plk_wy << setw(D) << fixed << setprecision(P) << A[i][j] << " ";
+			}
+			plk_wy << endl;
+		}
+		plk_wy.close();
 
-        return 0;
-        cout << "Plik \"daneR.txt\" nie istnieje." << endl;
-    }
+		return 0;
+		cout << "Plik \"daneR.txt\" nie istnieje." << endl;
+	}
 }
