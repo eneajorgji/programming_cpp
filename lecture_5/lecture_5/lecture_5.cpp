c47237c4212451e32e0002c4e500c2b0c4b684f0
Enea <jorgji.enea@gmail.com>
2023-01-21 22:44:31 +0100

    created zadanie 6 project

--- lecture_5/lecture_5/lecture_5.cpp
+++ lecture_5/lecture_5/lecture_5.cpp
@@ -3,43 +3,83 @@
 
 using namespace std;
 
-// d³ugoœæ tablicy punktów 
-const int n = 6;
-
-// pomocnicza funkcja wykorzystywana przez nasz program
-void czytaj_i_pisz(int dlugosc)
+// Definicja struktury danych
+struct LiczbaZespolona
 {
-    // tablica teraz bêdzie nam potrzebna jedynie wewn¹trz funkcji
-    int t[n];
-
-    // wczytanie ci¹gu 
-    cout << "******************************\n";
-    cout << "Podaj " << dlugosc << " liczb:\n";
-    for (int i = 0; i < dlugosc; i++)
-    {
-        cout << " Liczba " << i + 1 << " : ";
-        cin >> t[i];
-    };
-    // wydruk w odwrotnej kolejnoœci 
-    cout << "\nCiag w odwrotnej kolejnosci:\n";
-    for (int i = dlugosc - 1; i >= 0; i--)
-        cout << " Liczba " << i + 1 << " : " << t[i] << endl;
+	double re, im;
 };
+
+// Funkcja sumuj¹ca - parametry przekazywane przez wartoœæ
+LiczbaZespolona dodaj_wartosc(LiczbaZespolona x, LiczbaZespolona y)
+{
+	x.re += y.re;
+	x.im += y.im;
+
+	return x;
+}
+
+// Taka sama funkcja - lecz tym razem parametry przeka¿emy przez referencjê 
+LiczbaZespolona dodaj_zmienna(LiczbaZespolona& x, LiczbaZespolona& y)
+{
+	x.re += y.re;
+	x.im += y.im;
+
+	return x;
+}
+
+
 
 int main(int argc, char* argv[])
 {
-    cout << "Program procedury_2\n\n";
+	cout << "Roznica w sposobie przekazywania parametrow\n\n";
 
+	// liczby bêd¹ce naszymi argumentami: 
+	LiczbaZespolona a, b, c;
+	// wynik dodawania: 
+	LiczbaZespolona d;
 
-    // Pierwszy ci¹g o 5 liczbach 
-    czytaj_i_pisz(5);
+	a.re = 1;
+	a.im = 2;
+	b.re = 3;
+	b.im = 4;
+	c.re = 5;
+	c.im = 6;
 
-    // Drugi o 6 liczbach 
-    czytaj_i_pisz(6);
+	cout << "Parametry przekazywane przez wartosc\n\n";
+	cout << "Liczby przed wywolaniem funkcji dodaj_wartosc:\n";
+	cout << "  a = " << a.re << "+" << a.im << "*i\n";
+	cout << "  b = " << b.re << "+" << b.im << "*i\n";
+	cout << "  c = " << c.re << "+" << c.im << "*i\n";
 
-    // i trzeci o 3 liczbach 
-    czytaj_i_pisz(3);
+	// dodanie a + b i a + c przy pomocy pierwszej wersji funkcji: 
+	cout << "Wynik dodawania:\n";
+	d = dodaj_wartosc(a, b);
+	cout << "  a + b = " << d.re << "+" << d.im << "*i\n";
+	d = dodaj_wartosc(a, c);
+	cout << "  a + c = " << d.re << "+" << d.im << "*i\n";
 
+	cout << "Liczby po wywolaniu funkcji dodaj_wartosc:\n";
+	cout << "  a = " << a.re << "+" << a.im << "*i\n";
+	cout << "  b = " << b.re << "+" << b.im << "*i\n";
+	cout << "  c = " << c.re << "+" << c.im << "*i\n";
 
-    return 0;
+	cout << "Parametry przekazywane przez zmienna\n\n";
+	cout << "Liczby przed wywolaniem funkcji dodaj_zmienna:\n";
+	cout << "  a = " << a.re << "+" << a.im << "*i\n";
+	cout << "  b = " << b.re << "+" << b.im << "*i\n";
+	cout << "  c = " << c.re << "+" << c.im << "*i\n";
+
+	// dodanie a + b i a + c przy pomocy pierwszej wersji funkcji: 
+	cout << "Wynik dodawania:\n";
+	d = dodaj_zmienna(a, b);
+	cout << "  a + b = " << d.re << "+" << d.im << "*i\n";
+	d = dodaj_zmienna(a, c);
+	cout << "  a + c = " << d.re << "+" << d.im << "*i\n";
+
+	cout << "Liczby po wywolaniu funkcji dodaj_zmienna:\n";
+	cout << "  a = " << a.re << "+" << a.im << "*i\n";
+	cout << "  b = " << b.re << "+" << b.im << "*i\n";
+	cout << "  c = " << c.re << "+" << c.im << "*i\n";
+
+	return 0;
 }
