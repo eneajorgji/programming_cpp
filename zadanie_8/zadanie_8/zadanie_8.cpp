2d871761b1da638acd7f36173f35fa71221f93e7
Enea <jorgji.enea@gmail.com>
2023-01-23 23:00:41 +0100

    completed point 1, 2, 3

--- zadanie_8/zadanie_8/zadanie_8.cpp
+++ zadanie_8/zadanie_8/zadanie_8.cpp
@@ -1,20 +1,80 @@
-// zadanie_8.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <iomanip>
 
-int main()
+using namespace std;
+
+struct Sstudent
 {
-    std::cout << "Hello World!\n";
+    char inicjal_1, inicjal_2;
+    double ocena;
+};
+
+void losowanie(Sstudent A[], int n) {
+    const double G = 5.0;
+    //srand(time(0));
+
+    for (int i = 0; i < n; i++) {
+        A[i].inicjal_1 = static_cast<char>('A' + rand() % 26);
+        A[i].inicjal_2 = static_cast<char>('A' + rand() % 26);
+        A[i].ocena = static_cast<double>(rand()) / static_cast<double>(RAND_MAX / G);
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void drukowanie(Sstudent A[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << A[i].inicjal_1 << A[i].inicjal_2 << " " << fixed << setprecision(2) << A[i].ocena << " ";
+    }
+}
+double max(Sstudent A[], int n) {
+    double tmp_max = 0.0;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    for (int i = 0; i < n; i++) {
+        if (tmp_max < A[i].ocena) {
+            tmp_max = A[i].ocena;
+        }
+    }
+    return tmp_max;
+}
+
+double srednia(Sstudent A[], int n) {
+    double tmp_sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        tmp_sum = tmp_sum + A[i].ocena;
+    }
+    return tmp_sum / n;
+}
+
+int main() {
+    const int n = 4;
+    Sstudent G1[n];
+    Sstudent G2[n];
+
+    srand(time(0));
+
+    // losowanie
+    losowanie(G1, n);
+    losowanie(G2, n);
+
+    // drukowanie
+    cout << "G1" << endl;
+    drukowanie(G1, n);
+    cout << endl;
+    cout << "G2" << endl;
+    drukowanie(G2, n);
+
+    // ró¿nica maxów
+    cout << endl;
+    cout << "roznica maxow wynosi: " << abs(max(G1, n) - max(G2, n)) << endl;
+    double tmp1 = srednia(G1, n), tmp2 = srednia(G2, n);
+    //cout<<"tmp1 "<<tmp1<<endl;
+    //cout<<"tmp2 "<<tmp2<<endl;
+    if (tmp1 > tmp2) cout << "wieksza jest srednia w G1 i wynosi: " << srednia(G1, n);
+    if (tmp1 < tmp2) cout << "wieksza jest srednia w G2 i wynosi: " << srednia(G2, n);
+    if (tmp1 = tmp2) cout << " srednie sa rowne i  wynosza: " << srednia(G1, n);
+
+    return 0;
+}
